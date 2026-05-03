0a2a2ae0c60b550b2bf9f21d4c1eb62b180c75de
Jede Jagadeesh <jagadeeshjade490@gmail.com>
2026-05-03 21:10:16 +0530

    cors fix

--- bus_booking_frontend/src/api/api.js
+++ bus_booking_frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://onlinebusticketreservationsystem-production.up.railway.app/api/",
+  baseURL: "https://onlinebusticketreservationsystem-production.up.railway.app/api"
 });
 
 // ================= BUSES =================
